a1f0a07c757287a804589a65694447c1558bd0bb
icantcodefyi <venti.sillly@gmail.com>
2025-02-07 03:28:07 +0530

    woah posthog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,13 +6,25 @@ import { type Metadata } from "next";
 import { TRPCReactProvider } from "@/trpc/react";
 import { Toaster } from "@/components/ui/toaster";
 import { SessionProviderWrapper } from "@/app/_components/session-provider";
+import { CSPostHogProvider } from "@/app/_components/posthog-provider";
+
 export const metadata: Metadata = {
   title: "Diagramify by Ani",
   description: "Generate diagrams with AI",
   icons: [
     { rel: "icon", url: "/favicon.ico" },
-    { rel: "icon", url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-    { rel: "icon", url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    {
+      rel: "icon",
+      url: "/favicon-16x16.png",
+      sizes: "16x16",
+      type: "image/png",
+    },
+    {
+      rel: "icon",
+      url: "/favicon-32x32.png",
+      sizes: "32x32",
+      type: "image/png",
+    },
     { rel: "apple-touch-icon", url: "/apple-touch-icon.png" },
   ],
   manifest: "/site.webmanifest",
@@ -25,8 +37,10 @@ export default function RootLayout({
     <html lang="en" className={`${GeistSans.variable}`}>
       <body>
         <TRPCReactProvider>
-          <SessionProviderWrapper>{children}</SessionProviderWrapper>
-          <Toaster />
+          <CSPostHogProvider>
+            <SessionProviderWrapper>{children}</SessionProviderWrapper>
+            <Toaster />
+          </CSPostHogProvider>
         </TRPCReactProvider>
       </body>
     </html>
